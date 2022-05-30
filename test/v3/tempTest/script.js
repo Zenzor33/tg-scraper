cd41da165a529eb479910b0564dfcf080a3db4a6
Zenzor33 <rzeidler@gmail.com>
2022-05-30 14:43:32 -0300

    bug: can't append messages to users who already exist in object

--- test/v3/tempTest/script.js
+++ test/v3/tempTest/script.js
@@ -1,32 +1,16 @@
 // RICH'S RUBE GOLDBERG MACHINE
 
-/*
-This program will make a new object for each user. Each object will have two properties. A property called 'author' that contains the name of the user, and a property called 'messages' that contains an array of all the user's messages. 
-
-The program should be designed to scale for the next version which tracks date of each message.
-
-The program will perform one loop through the html content. The loop will check:
-- const isDifferentUserDiv =  If the message is written by a different user than the previous message
--- If (isDifferentUserDiv), program:
---- logs name of the user to keep track of currentUser
---- Checks if the user already has an object. 
------ If false, create new object & append message.
------ If true, append message
--- If (!isDifferentUserDiv), program:
---- appends message to the previous detected user.
-*/
-
 const container = document.querySelector(".history");
-let tempArray = [{}, {}];
+const users = {};
 
 const classNames = {
   isDifferentUserDiv: "message default clearfix",
-  isSameUser: "message default clearfix joined",
+  isSameUserDiv: "message default clearfix joined",
   checkIsDifferentUserDiv(className) {
     return className === this.isDifferentUserDiv ? true : false;
   },
-  checkIsSameUser(className) {
-    return className === this.isSameUser ? true : false;
+  checkIsSameUserDiv(className) {
+    return className === this.isSameUserDiv ? true : false;
   },
 };
 
@@ -35,35 +19,37 @@ const getMessage = (childElement) => {
   if (element !== null) return element.outerText;
 };
 
+const updateSubObject = (currentUser, currentMessage, element) => {
+  // check if user exists in object
+  let userExists = users[currentUser];
+  if (!userExists) {
+    // create subobject with user & current message
+    currentUserObj = new User(currentUser, currentMessage);
+    users[currentUser] = currentUserObj;
+    users[currentUser].pushMessage(currentMessage);
+  } else if (userExists) {
+    users[currentUser].pushMessage(currentMessage);
+  }
+};
+
 const fetchUserMessages = function (user) {
   const childElementsLength = container.children.length;
   for (let i = 0; i < childElementsLength; i++) {
     let childElement = container.children[i];
     let currentUser = "";
-    let isSameUser = classNames.checkIsSameUser(childElement.className);
+    let isSameUserDiv = classNames.checkIsSameUserDiv(childElement.className);
     let isDifferentUserDiv = classNames.checkIsDifferentUserDiv(
       childElement.className
     );
 
-    if (isDifferentUserDiv) {
+    if (isDifferentUserDiv || isSameUserDiv) {
       // update currentUser
       currentUser = getNewUserName(childElement);
       currentMessage = getMessage(childElement);
-      // check if user exists in array
-      let userExists = Boolean(tempArray.find((o) => o.name === currentUser));
-      if (!userExists) {
-        // if user does not exist in array, push object with user and message
-        tempArray.push({ name: currentUser, message: [currentMessage] });
-      } else if (userExists) {
-        // append current message to user's message's array in object
-      }
+      updateSubObject(currentUser, currentMessage, childElement);
     }
   }
 };
-
-//
-// IGNORE THE CODE BLOW. IT WAS FOR TESTING
-//
 
 // create object constructor
 function User(author, message) {
@@ -76,65 +62,11 @@ User.prototype.pushMessage = function (message) {
   this.message.push(message);
 };
 
-// create object
-const richard = new User("richard");
-// push message to object
-richard.pushMessage("hello");
-// push object to array
-tempArray.push(richard);
-
 const getNewUserName = (element) =>
   element.querySelector(".from_name")?.innerText;
 
-// const test = Array.from(document.querySelectorAll(".history .body")).map(
-//   (element) => {
-//     return {
-//       author: element.querySelector(".from_name")?.innerText,
-//       message: element.querySelector(".text")?.innerText,
-//     };
-//   }
-// );
-
-// const container = document.querySelector(".history");
-// const obj = {
-//   author: "",
-//   messages: [],
-// };
-
-// const getMessage = (childElement) => {
-//   const element = childElement.querySelector(".text");
-//   if (element !== null) return element.outerText;
-// };
-
-// const updateObject = (author, message) => {
-//   obj["author"] = author;
-//   obj["messages"].push(message);
-// };
-
-// const fetchUserMessages = function (desiredAuthor) {
-//   const childElementsLength = container.children.length;
-//   let newAuthor = "";
-
-//   for (let i = 0; i < childElementsLength; i++) {
-//     let childElement = container.children[i];
-//     let authorDiv = childElement.querySelector(".from_name");
-
-//     const isNewAuthor = childElement.className === "message default clearfix";
-// const isSameAuthor =
-//   childElement.className === "message default clearfix joined";
-
-//     if (isNewAuthor) newAuthor = authorDiv.outerText;
-
-//     if (
-//       (isNewAuthor && newAuthor === desiredAuthor) ||
-//       (isSameAuthor && newAuthor === desiredAuthor)
-//     ) {
-//       let message = getMessage(childElement);
-//       updateObject(desiredAuthor, message);
-//     }
-//   }
-//   console.log(obj);
-// };
+fetchUserMessages();
+console.log(users);
 
 ("use strict");
 
